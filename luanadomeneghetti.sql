3141cb6e6822f106752bb36cf40d997cb23f6272
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-05 10:26:59 -0300

    create: inserts

--- luanadomeneghetti.sql
+++ luanadomeneghetti.sql
@@ -36,9 +36,17 @@ CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
 INSERT INTO hospedes (nome, email) VALUES
 ('João', 'joao@hotel.com'),
 ('Maria', 'maria@hotel.com'),
-('Pedro', 'pedro@hotel.com'),
+('Pedro', 'pedro@hotel.com'), 
+('Ana', 'ana@hotel.com');
 
 INSERT INTO quartos (servico_quarto, tipo_cama, bloco) VALUES   
-('Serviço 1', 'Cama 1', 1),
-('Serviço 1', 'Cama 2', 2),
-('Serviço 1', 'Cama 3', 3),
+('Organização Geral', 'Cama Padma', 1),
+('Limpeza Diária', 'Cama Nórdica', 2),
+('Camareira', 'Cama Ribas', 3),
+('Concierge', 'Cama Ellen', 4);
+
+INSERT INTO reservas (data_inicio, data_termino, data_reserva, horario_reserva, id_hospede, id_quarto) VALUES
+('2024-11-10', '2024-11-14', '2024-10-28', '09:45', 1, 1),
+('2024-12-19', '2024-12-23', '2024-11-30', '13:00', 2, 2),
+('2024-12-25', '2024-12-28', '2024-12-05', '11:15', 3, 3),
+('2024-01-18', '2024-01-22', '2024-12-31', '16:', 4, 4);
