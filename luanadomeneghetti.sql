5c73d846df3fd574fae7649136b597ada328469c
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-05 09:19:53 -0300

    create: reservation table.

--- luanadomeneghetti.sql
+++ luanadomeneghetti.sql
@@ -16,3 +16,15 @@ servico_quarto VARCHAR(100),
 tipo_cama VARCHAR(140),
 bloco INT
 );
+
+SELECT * FROM quartos;
+
+CREATE TABLE reservas (
+id_reserva SERIAL PRIMARY KEY,
+data_reserva DATE NOT NULL DEFAULT CURRENT_DATE,
+hora_reserva TIME NOT NULL,
+id_hospede INT NOT NULL,
+id_quarto INT NOT NULL,
+CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
+);
