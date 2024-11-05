527256d103395ef952b5150b4ad02c06d7821390
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-05 09:12:30 -0300

    create: bedroom table.

--- luanadomeneghetti.sql
+++ luanadomeneghetti.sql
@@ -7,3 +7,12 @@ id_hospede SERIAL PRIMARY KEY,
 nome VARCHAR(100) NOT NULL,
 email VARCHAR(200) UNIQUE NOT NULL
 );
+
+SELECT * FROM hospedes;
+
+CREATE TABLE quartos (
+id_quarto SERIAL PRIMARY KEY,
+servico_quarto VARCHAR(100),
+tipo_cama VARCHAR(140),
+bloco INT
+);
