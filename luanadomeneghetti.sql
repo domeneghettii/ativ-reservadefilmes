42ed748de2a4d39b026f7c361bb8620ceb5ade3c
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-11-05 09:35:16 -0300

    feat: adjustments.

--- luanadomeneghetti.sql
+++ luanadomeneghetti.sql
@@ -22,10 +22,23 @@ SELECT * FROM quartos;
 CREATE TABLE reservas (
 id_reserva SERIAL PRIMARY KEY,
 data_inicio DATE NOT NULL DEFAULT CURRENT_DATE,
-hora_termino DATE NOT NULL DEFAULT CURRENT_TIME,
+data_termino DATE NOT NULL DEFAULT CURRENT_DATE,
+data_reserva DATE NOT NULL DEFAULT CURRENT_DATE,
+horario_reserva TIME NOT NULL,
 id_hospede INT NOT NULL,
 id_quarto INT NOT NULL,
 CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
 CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
 );
 
+\dt
+
+INSERT INTO hospedes (nome, email) VALUES
+('João', 'joao@hotel.com'),
+('Maria', 'maria@hotel.com'),
+('Pedro', 'pedro@hotel.com'),
+
+INSERT INTO quartos (servico_quarto, tipo_cama, bloco) VALUES   
+('Serviço 1', 'Cama 1', 1),
+('Serviço 1', 'Cama 2', 2),
+('Serviço 1', 'Cama 3', 3),
